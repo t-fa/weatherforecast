0e83b864edbfc5a17cfc7cb8a33802cf15c5c1e5
t-fa <thomas.fattah@gmail.com>
2020-01-19 18:14:28 -0500

    Added Input class

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    weather: []
   }
   render () {
     return (
-      <div className="App">
+      <div className="container">
         <h1>5 Day Weather Forecast</h1>
         <Weather />
       </div>
     );
   }
+}
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <form>
+        <div className="form-group">
+          <label for="zipcode">Zip Code:</label>
+          <input type="text" className="form-control w-50 p-3" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    );
+  };
 }
 
 class Weather extends React.Component {
@@ -22,10 +39,18 @@ class Weather extends React.Component {
   }
   render() {
     return (
-      <h2>Location Name</h2>
-      <div className="container">
+      <div>
+        <Input />
+        <h2>Location Name</h2>
         <div className="row">
-          <Day day="Monday" condition="sunny"/>
+          <Day 
+          day="Monday" 
+          condition="sunny"
+          currenttemp="70"
+          rain="22%"
+          high="80"
+          low="34"
+          />
           <Day day="Tuesday" condition="rainy"/>
           <Day day="Wednesday" condition="cloudy"/>
           <Day day="Thursday" condition="thunderstorm"/>
@@ -47,11 +72,11 @@ class Day extends React.Component {
           {this.props.day}
           <div className="card-subtitle">
             {this.props.condition}
+            {this.props.currenttemp}
+            {this.props.rain}
+            {this.props.high}
+            {this.props.low}
           </div>
-          {this.props.currenttemp}
-          {this.props.rain}
-          {this.props.high}
-          {this.props.low}
         </div>
       </div>
     );
