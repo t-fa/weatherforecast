cae545a559b2aa98467e294bc275ffbcba988a86
t-fa <thomas.fattah@gmail.com>
2020-01-19 16:59:08 -0500

    Converted App to class and created Weather class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render () {
+    return (
+      <div className="App">
+        <h1>5 Day Weather Forecast</h1>
+        <Weather />
+      </div>
+    );
+  }
+}
 
-    </div>
-  );
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col border">
+            1 of 3
+          </div>
+          <div className="col border">
+            2 of 3
+          </div>
+          <div className="col border">
+            3 of 3
+          </div>
+          <div className="col border">
+            4 of 3
+          </div>
+          <div className="col border">
+            5 of 3
+          </div>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default App;
