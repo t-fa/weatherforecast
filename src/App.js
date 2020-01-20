f9adc98b4f698372c9402eae026f29cf017adcc1
t-fa <thomas.fattah@gmail.com>
2020-01-20 15:25:49 -0500

    Added submitZip() and passed weather data as prop to Weather component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const APIKEY = require('./config.js');
+
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      zip: ''
+      zip: '',
+      weather: []
     }
     this.getZip = this.getZip.bind(this);
+    this.submitZip = this.submitZip.bind(this);
   }
 
   getZip(event) {
     this.setState({
       zip: event.target.value
     });
+  }
+
+  submitZip(event){
+    event.preventDefault();
+    let url = `http://api.openweathermap.org/data/2.5/weather?zip=${this.state.zip},us&units=imperial&APPID=${APIKEY.APIKEY}`
+    fetch(url)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ weather: data })
+      console.log(this.state.weather);
+    })
+    .catch(console.log)
   }
 
   render () {
@@ -26,9 +42,11 @@ class App extends React.Component {
             <label for="zipcode">Zip Code:</label>
             <input type="text" className="form-control w-50 p-3" value={this.state.zip} onChange={this.getZip}/>
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary" onClick={this.submitZip}>Submit</button>
         </form>
-        <Weather />
+        <Weather 
+        weather={this.state.weather}
+        />
       </div>
     );
   }
@@ -41,7 +59,7 @@ class Weather extends React.Component {
   render() {
     return (
       <div>
-        <h2>Location Name</h2>
+        <h2>Weather for: {this.props.weather.name}</h2>
         <div className="row">
           <Day 
           day="Monday" 
